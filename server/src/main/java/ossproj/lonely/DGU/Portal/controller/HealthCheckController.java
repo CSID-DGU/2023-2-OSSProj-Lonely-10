ac21ddb0da54cb21812e1d7a9c897e0b9b44250e
kurtyoon <akakertcobain@naver.com>
2023-11-04 14:36:29 +0900

    Feat: 서버 상태 체크 컨트롤러 수정

--- server/src/main/java/ossproj/lonely/DGU/Portal/controller/HealthCheckController.java
+++ server/src/main/java/ossproj/lonely/DGU/Portal/controller/HealthCheckController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/v1")
 @Tag(name = "HealthCheck", description = "Health Check API")
 public class HealthCheckController {
     @Operation(summary = "Health Check", description = "Health Check")

6342b9a5ac9937620e7283f0bf33683b308a932f
kurtyoon <akakertcobain@naver.com>
2023-11-04 14:39:02 +0900

    Feat: 잘못된 사용자, 로그인 실패, 잘못된 요청 등에 대한 에러 함수 작성

--- server/src/main/java/ossproj/lonely/DGU/Portal/exception/ErrorCode.java
+++ server/src/main/java/ossproj/lonely/DGU/Portal/exception/ErrorCode.java
@@ -7,6 +7,10 @@ import org.springframework.http.HttpStatus;
 public enum ErrorCode {
 
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, "USER NOT FOUND"),
+    LOGIN_FAILED(HttpStatus.BAD_REQUEST, "LOGIN FAILED"),
+    DUPLICATE_USER(HttpStatus.BAD_REQUEST, "DUPLICATE USER"),
+    INVALID_REQUEST(HttpStatus.BAD_REQUEST, "INVALID REQUEST"),
+    WRONG_PASSWORD(HttpStatus.BAD_REQUEST, "WRONG PASSWORD"),
 
     ;
 

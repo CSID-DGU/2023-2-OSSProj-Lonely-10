667e70f7af4539ed3ca2adbd11b21e960813dc50
kurtyoon <akakertcobain@naver.com>
2023-11-12 17:27:39 +0900

    Feat: 강좌 찾기 기능 구현

--- server/src/main/java/ossproj/lonely/DGU/Portal/service/CourseService.java
+++ server/src/main/java/ossproj/lonely/DGU/Portal/service/CourseService.java
@@ -5,11 +5,13 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import ossproj.lonely.DGU.Portal.domain.Course;
+import ossproj.lonely.DGU.Portal.dto.enrollment.response.GetCourseResponseDto.SubCourseDto;
 import ossproj.lonely.DGU.Portal.dto.main.sub.CourseDto;
 import ossproj.lonely.DGU.Portal.repository.CourseRepository;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Service
@@ -20,6 +22,11 @@ public class CourseService {
     @Transactional
     public void save(Course course) {
         courseRepository.save(course);
+    }
+
+    @Transactional
+    public Course findByCourseCode(String courseCode) {
+        return courseRepository.findByCourseCode(courseCode);
     }
 
     @Transactional
@@ -39,5 +46,27 @@ public class CourseService {
         courseDtos.add(course5);
 
         return courseDtos;
+    }
+
+    @Transactional
+    public List<SubCourseDto> getCourseByCourseName(String courseName) {
+        List<Course> courses = courseRepository.findByCourseName(courseName);
+        return courses.stream()
+                .map(course -> new SubCourseDto(course.getCourseName(), course.getCourseCode(), course.getProfessor(), course.isOnline()))
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public List<SubCourseDto> getCourseByCourseCode(String courseCode) {
+        Course course = courseRepository.findByCourseCode(courseCode);
+        return List.of(new SubCourseDto(course.getCourseName(), course.getCourseCode(), course.getProfessor(), course.isOnline()));
+    }
+
+    @Transactional
+    public List<SubCourseDto> getCourseByProfessor(String professor) {
+        List<Course> courses = courseRepository.findByProfessor(professor);
+        return courses.stream()
+                .map(course -> new SubCourseDto(course.getCourseName(), course.getCourseCode(), course.getProfessor(), course.isOnline()))
+                .collect(Collectors.toList());
     }
 }

bb04beae20f93bea49297b7d32fe7e13351053db
kurtyoon <akakertcobain@naver.com>
2023-11-19 02:34:49 +0900

    Feat: JPQL 쿼리를 통해 도메인을 불러오도록 설정

--- server/src/main/java/ossproj/lonely/DGU/Portal/repository/AssignmentRepository.java
+++ server/src/main/java/ossproj/lonely/DGU/Portal/repository/AssignmentRepository.java
@@ -1,7 +1,12 @@
 package ossproj.lonely.DGU.Portal.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import ossproj.lonely.DGU.Portal.domain.Assignment;
 
+import java.util.List;
+
 public interface AssignmentRepository extends JpaRepository<Assignment, Long> {
+    @Query("SELECT a FROM Assignment a WHERE a.course.courseCode = :courseCode")
+    List<Assignment> findByCourseCode(String courseCode);
 }
--- server/src/main/java/ossproj/lonely/DGU/Portal/repository/ScoreRepository.java
+++ server/src/main/java/ossproj/lonely/DGU/Portal/repository/ScoreRepository.java
@@ -1,7 +1,12 @@
 package ossproj.lonely.DGU.Portal.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import ossproj.lonely.DGU.Portal.domain.Score;
 
+import java.util.List;
+
 public interface ScoreRepository extends JpaRepository<Score, Long> {
+    @Query("SELECT a FROM Score a WHERE a.enrollment.user.userCode = :userCode AND a.enrollment.course.courseCode = :courseCode")
+    List<Score> findScoreByUserCodeAndCourseCode(String userCode, String courseCode);
 }

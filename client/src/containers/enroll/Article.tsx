844500e9ebc6455ad006c23f778bb2920c403704
sayyyho <323psh@naver.com>
2023-12-01 19:49:11 +0900

    fix: client 연동 오류 체크

---
+++ client/src/containers/enroll/Article.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import styles from "./table.module.css";
+
+const Article = () => {
+  return (
+    <>
+      <h3>수강 신청 페이지</h3>
+    </>
+  );
+};
+
+export default Article;
--- client/src/containers/enroll/index.tsx
+++ client/src/containers/enroll/index.tsx
@@ -1,5 +1,22 @@
+import Article from "./Article";
+import Greeting from "@/components/Greeting";
+import styles from "./enroll.module.css";
+const userCode = "2019112127";
+const userName = "박세호";
 const Enroll = () => {
-  return <></>;
+  return (
+    <div className={styles.layout}>
+      <div className={styles.leftFrame}>
+        <Greeting
+          userName={userName ? userName : "이름오류"}
+          userCode={userCode ? userCode : "학번오류"}
+        ></Greeting>
+      </div>
+      <div className={styles.frame}>
+        <Article></Article>
+      </div>
+    </div>
+  );
 };
 
 export default Enroll;

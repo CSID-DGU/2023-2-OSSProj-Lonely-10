b451ce446d885ac5e01fd6773e6798e1133cdc0c
sayyyho <323psh@naver.com>
2023-12-02 18:30:05 +0900

    feat: 다음 스텝

--- client/src/containers/enroll/id/index.tsx
+++ client/src/containers/enroll/id/index.tsx
@@ -31,37 +31,41 @@ const ConfirmEnroll = () => {
     // 완료됐다는 어떤 시그널이 있을 때, useEffect 한 번씩 불러줘야함
   }, []);
 
-  const handleCancel = (courseCode: string, courseName: string) => {
+  const handleCancel = async (courseCode: string, courseName: string) => {
     console.log(courseCode, userCode);
-    Swal.fire({
+    const result = await Swal.fire({
       title: `${courseName}과목 (${courseCode})을 수강 취소하시겠습니까?`,
       text: "취소 후 다시 되돌릴 수 없습니다.",
       icon: "warning",
       showConfirmButton: true,
+      showCancelButton: true,
       timerProgressBar: true,
       confirmButtonText: "수강 취소",
+      cancelButtonText: "X",
       confirmButtonColor: "#d33",
-    }).then((result) => {
-      if (result.isConfirmed) {
-        axios
-          .delete(`http://localhost/api/v1/enroll/`, {
-            headers: {
-              Authorization: auth,
-            },
-            data: {
-              user_code: userCode,
-              course_code: courseCode,
-            },
-          })
-          .then((res) => {
-            Swal.fire(
-              `${courseName} 과목이 수강 취소됐습니다.`,
-              `학수번호 ${courseCode}`,
-              "success"
-            );
-          });
-      }
     });
+
+    if (result.isConfirmed) {
+      try {
+        const response = await axios.delete(`http://localhost/api/v1/enroll/`, {
+          headers: {
+            Authorization: auth,
+          },
+          data: {
+            user_code: userCode,
+            course_code: courseCode,
+          },
+        });
+        console.log(response);
+        Swal.fire(
+          `${courseName} 과목이 수강 취소됐습니다.`,
+          `학수번호 ${courseCode}`,
+          "success"
+        );
+      } catch (error) {
+        console.log(error);
+      }
+    }
   };
 
   return (

bae9ee1f7e7716a451aa42cd26144f13d85fd2bf
kurtyoon <akakertcobain@naver.com>
2023-12-08 22:18:51 +0900

    Fix: Enrollment Query 추가

--- server/src/main/java/ossproj/lonely/DGU/Portal/repository/AttendanceRepository.java
+++ server/src/main/java/ossproj/lonely/DGU/Portal/repository/AttendanceRepository.java
@@ -2,11 +2,13 @@ package ossproj.lonely.DGU.Portal.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import ossproj.lonely.DGU.Portal.domain.Attendance;
 import ossproj.lonely.DGU.Portal.domain.Enrollment;
 
 import java.util.List;
 
 public interface AttendanceRepository extends JpaRepository<Attendance, Long> {
-    List<Attendance> findAttendanceByEnrollment(Enrollment enrollment);
+    @Query("select a from Attendance a where a.enrollment = :enrollment")
+    List<Attendance> findAttendanceByEnrollment(@Param("enrollment") Enrollment enrollment);
 }
